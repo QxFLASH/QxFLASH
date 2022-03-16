eef66d94058e6161ff4a535b8fba5cbf9d841247
QxFLASH_BOT <101647982+QxFLASH@users.noreply.github.com>
2022-03-16 09:39:28 +0530

    Create tagadmin.js

---
+++ tagadmin.js
@@ -0,0 +1,36 @@
+let Bot = require('../events');
+let {MessageType} = require('@adiwajshing/baileys');
+let Config = require('../config');
+let Language = require('../language');
+let Lang = Language.getString('tagall');
+
+if (Config.WORKTYPE == 'private') {
+    
+    Bot.addCommand({pattern: 'tagadmin$', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '▫️@' + uye.id.split('@')[0] + '\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+    
+    Bot.addCommand({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '▫️@' + uye.id.split('@')[0] + '\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+}
